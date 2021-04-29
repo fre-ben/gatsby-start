24e142a2535e8e0f61dbb9b18b53104872a662a6
fre-ben <fschocke@gmail.com>
2021-04-29 17:20:12 +0200

    3. Creating Nested Layout Components

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,5 +15,11 @@ module.exports = {
         allExtensions: true, // defaults to false
       },
     },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
   ],
 }
