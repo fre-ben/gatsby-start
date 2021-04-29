923986c6c78476cce79f5d2c272a064183396124
fre-ben <fschocke@gmail.com>
2021-04-29 19:38:55 +0200

    4. Data in Gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,9 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: "Pandas Eating Lotz",
+  },
   plugins: [
-    {
-      resolve: `gatsby-plugin-typescript`,
-      options: {
-        isTSX: true, // defaults to false
-        jsxPragma: `jsx`, // defaults to "React"
-        allExtensions: true, // defaults to false
-      },
-    },
+    `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-plugin-typography`,
       options: {
--- src/components/container.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import * as styles from "./container.module.css"
-
-export default function Container({ children }) {
-  return <div className={styles.container}>{children}</div>
-}
